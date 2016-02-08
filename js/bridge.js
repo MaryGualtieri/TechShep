8e0173f897a4f3b9c4c4b0f0627e7d991b7849d8
Mary Gualtieri <mgualtieri7@gmail.com>
2016-02-08 08:38:31 -0500

    fixed bridge.js

--- js/bridge.js
+++ js/bridge.js
@@ -8,11 +8,11 @@ var keywords = search.split(" ");
 var matcharray = [];//final match with the lowest index combination
 var comparearray = [];//temp array used to collect full object
 var temparray = [];//temp array used to collect word comparison
-// var finalarray = [];//contains every combinations
+var finalarray = [];//contains every combinations
 var results = [];//words and indexes
 var tempindex = 0;
 var index = 0;
-var matchstrings = (" ");
+var runtime = 0;
 
 function matchingSearchwords(data,keywords) {
     for ( x = 0; x < data.length; x++) {
@@ -21,38 +21,54 @@ function matchingSearchwords(data,keywords) {
                 var keyword = new RegExp ( "^" + keywords[j].toLowerCase() + "$" );
                  if ( word.match(keyword) ) {
                     results.push( { word: data[x].toLowerCase(), index: x} );
+
             }
         }
     }
+
     return results;
 }
 
 var counter = 0;
 var ran = matchingSearchwords(data,keywords);
 
-
 function findmatch(array) {
-    var arr2 = array;
 
+    var arr2 = array;
     for(i = 0; i < array.length; i++) {
         if(comparearray.length > 2) {
-            var phrase = (datafull.slice( (comparearray[0].index-1), (comparearray[comparearray.length-2].index+1)) ).join(" ");
-            console.log(phrase);
+            var phrase = (datafull.slice( (comparearray[0].index-1), (comparearray[comparearray.length-1].index+1)) ).join(" ");
+            comparearray.push( { distance: tempindex });
+            comparearray.push( { phrase: phrase } );
+            finalarray.push(comparearray);
             }
+            // console.log(comparearray);
 
+
+        if( index === 0 ) { index = tempindex } else {
+            if( tempindex < index ) {
+                index = tempindex;
+                matcharray.push(comparearray);
+                // console.log(matcharray);
+                matchingString = (datafull.slice( (comparearray[0].index-1), (comparearray[comparearray.length-3].index-1))).join(" ");
+
+            }
+        }
           comparearray = [];
           temparray = [];
-
-        for( j= counter; j < arr2.length; j++) {
+        for(j=counter; j < arr2.length; j++) {
 
             var currentword = arr2[j].word;
             if (temparray.indexOf(currentword) == -1 ) {
                 temparray.push(arr2[j].word);
                 comparearray.push(arr2[j]);
+                tempindex = comparearray[comparearray.length-1].index - comparearray[0].index;
+
             }
         }
     counter++;
      }
+     return matchingString;
 }
 
-findmatch(ran);
+console.log(findmatch(ran));
